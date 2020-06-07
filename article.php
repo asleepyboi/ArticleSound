6eeed2f8a55542943d4a30f529755ecf552254f7
Sleepy Boyo <64079074+asleepyboi@users.noreply.github.com>
2020-06-07 23:36:19 +0000

    Update article.php
    cursed

--- article.php
+++ article.php
@@ -1,15 +1,4 @@
-<?php
- 
-$articleURL = $_GET["article"];
-$soundURL = $_GET["sound"];
-
 $data = file_get_contents($articleURL);
-//echo $data;
-$parts = parse_url($soundURL);
-parse_str($parts['query'], $query);
-$url = $query["v"];
-
-$a = 'https://www.youtube.com/embed/'.$url."?autoplay=1#t=0";
-echo "<iframe width='1' height='1' src='$a' frameborder='0' allowfullscreen></iframe>";
 echo $data;
-?>
+
+pls no
